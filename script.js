83edd7311c510c7362d6d8307d1e5640f71a70f0
Anas <anassust.scw@gmail.com>
2023-11-18 09:55:34 +0600

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let text = document.getElementsByClassName('txt');
+function backgroundColorChanger(color) {
+    document.body.style.backgroundColor = color;
+    
+    if (color = black) {
+        for (let elm of text) {
+            elm.style.color = "white";
+        }
+    }
+}
