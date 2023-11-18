839d5a05e40b2bb76dd82636c4e7b79e183fadbd
Anas <anassust.scw@gmail.com>
2023-11-18 10:32:07 +0600

    final

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ let text = document.getElementsByClassName('txt');
 function backgroundColorChanger(color) {
     document.body.style.backgroundColor = color;
     
-    if (color = black) {
+    if (color == 'black') {
         for (let elm of text) {
             elm.style.color = "white";
+        }
+    }else{
+        for (let elm of text) {
+            elm.style.color = "black";
         }
     }
 }
